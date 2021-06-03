2892a2eabf6a6cc3e0b90d793721a0357682507f
abhi_softieons <abhi.softieons@gmail.com>
2021-06-03 15:07:32 +0530

    change secreat key

--- config.js
+++ config.js
@@ -3,5 +3,11 @@ module.exports = {
   PORT: process.env.PORT || 5000,
 
   //secret key
-  secret_key: "DP6FjzVzXBtBq45zcazsDQrOh",
+  secret_key: "DP6FfijzVzXBtBq45zcazsDQrOhMdg",
 };
+
+
+//did you put this code on server?
+//Yes
+
+//deploy again ??? yes ok
